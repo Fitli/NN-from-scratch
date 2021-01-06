4dadb7c98b8479bb966fd06573d2902b10fdad30
Eva Klimentová <xkliment@fi.muni.cz>
2021-01-06 11:31:20 +0100

    Corrected output filename

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main() {
     vector<int> topology = vector<int>({784, 256, 128, 10});
     NeuralNetwork network(topology, relu, d_relu);
     network.learn("data/fashion_mnist_train_vectors.csv", "data/fashion_mnist_train_labels.csv", 100, 256);
-    network.label("data/fashion_mnist_test_vectors.csv", "data/actualTestPredictions.csv");
+    network.label("data/fashion_mnist_test_vectors.csv", "actualTestPredictions.csv");
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> diff = end-begin;
     cout << "Time to train the network: " << diff.count()/60 << " min\n";
