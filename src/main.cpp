444d74a0dc6abc6ab746f3b2132f88f792384851
Ivana Krumlová <xkrumlov@fi.muni.cz>
2021-01-08 19:56:03 +0100

    main.cpp: correct filename and better architecture

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,10 @@ using namespace std;
 
 int main() {
     auto begin = std::chrono::steady_clock::now();
-    vector<int> topology = vector<int>({784, 256, 128, 10});
+    vector<int> topology = vector<int>({784, 256, 128, 64, 10});
     NeuralNetwork network(topology, relu, d_relu);
     network.learn("data/fashion_mnist_train_vectors.csv", "data/fashion_mnist_train_labels.csv", 100, 256);
-    network.label("data/fashion_mnist_test_vectors.csv", "actualTestPredictions.csv");
+    network.label("data/fashion_mnist_test_vectors.csv", "actualPredictions");
     auto end = std::chrono::steady_clock::now();
     std::chrono::duration<double> diff = end-begin;
     cout << "Time to train the network: " << diff.count()/60 << " min\n";
